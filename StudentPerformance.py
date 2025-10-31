4041edcfc71c56a5da48714eefbba6331232f61e
adawiyahh <adawiyah2k06@gmail.com>
2025-10-31 23:37:58 +0800

    Update StudentPerformance.py

--- StudentPerformance.py
+++ StudentPerformance.py
@@ -12,13 +12,13 @@ st.subheader("Dataset: Student Performance Metrics")
 
 # Load dataset from GitHub 
 # Replace this URL with your actual raw CSV file link
-csv_url = "https://raw.githubusercontent.com/adawiyahh/sciviz2025/refs/heads/main/law_faculty_survey.csv"
+csv_url = "https://raw.githubusercontent.com/adawiyahh/Assignment_JIE42303/refs/heads/main/ResearchInformation3.csv"
 col1, col2, col3, col4 = st.columns(4)
     
-col1.metric(label="PLO 2", value=f"3.3", help="PLO 2: Cognitive Skill", border=True)
-col2.metric(label="PLO 3", value=f"3.5", help="PLO 3: Digital Skill", border=True)
-col3.metric(label="PLO 4", value=f"4.0", help="PLO 4: Interpersonal Skill", border=True)
-col4.metric(label="PLO 5", value=f"4.3", help="PLO 5: Communication Skill", border=True)
+col1.metric(label="🎓 Average CGPA", value=f"{avg_cgpa}")
+col2.metric(label="📚 Avg Study Preparation (hrs)", value=f"{avg_prep}")
+col3.metric(label="🎮 Avg Gaming Time (hrs)", value=f"{avg_gaming}")
+col4.metric(label="🏫 Avg Attendance", value=f"{avg_attendance}")
 
 # Read dataset directly from GitHub
 law_df = pd.read_csv(csv_url)
