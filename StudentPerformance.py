7f79266d04a2daa49862e8ad5a92e57b73446698
adawiyahh <adawiyah2k06@gmail.com>
2025-11-02 05:04:53 +0800

    Update StudentPerformance.py

--- StudentPerformance.py
+++ StudentPerformance.py
@@ -7,27 +7,26 @@ import seaborn as sns
 def app():
     st.title("Student Performance")
 
-# Streamlit Page Setup 
-st.set_page_config(page_title="Assignment JIE42303 Scientific Visualization")
+    st.header("Assignment JIE42303 Scientific Visualization", divider="gray")
+    st.subheader("Dataset: Student Performance Metrics")
 
-st.header("Assignment JIE42303 Scientific Visualization", divider="gray")
-st.subheader("Dataset: Student Performance Metrics")
+    # Load Dataset from GitHub
+    csv_url = "https://raw.githubusercontent.com/adawiyahh/Assignment_JIE42303/refs/heads/main/ResearchInformation3_cleaned.csv"
+    df = pd.read_csv(csv_url)
 
-# Load Dataset from GitHub
-csv_url = "https://raw.githubusercontent.com/adawiyahh/Assignment_JIE42303/refs/heads/main/ResearchInformation3_cleaned.csv"
-df = pd.read_csv(csv_url)
+    # Display metrics
+    col1, col2, col3, col4 = st.columns(4)
 
-col1, col2, col3, col4 = st.columns(4)
+    avg_cgpa = df['Overall'].mean() if 'Overall' in df else 0
+    avg_attendance = df['Attendance'].mean() if 'Attendance' in df else 0
+    avg_prep = df['Preparation'].mean() if 'Preparation' in df else 0
+    avg_gaming = df['Gaming'].mean() if 'Gaming' in df else 0
 
-avg_cgpa = df['Overall'].mean() if 'Overall' in df else 0
-avg_attendance = df['Attendance'].mean() if 'Attendance' in df else 0
-avg_prep = df['Preparation'].mean() if 'Preparation' in df else 0
-avg_gaming = df['Gaming'].mean() if 'Gaming' in df else 0
+    col1.metric(label="🎓 Average CGPA", value=f"{avg_cgpa:.2f}", help="Average of all students’ cumulative CGPA")
+    col2.metric(label="📚 Average Attendance", value=f"{avg_attendance:.1f}", help="Average attendance rate among students")
+    col3.metric(label="📝 Average Study Preparation", value=f"{avg_prep:.1f}", help="Average study preparation level")
+    col4.metric(label="🎮 Average Gaming Time", value=f"{avg_gaming:.1f}", help="Average daily gaming hours")
 
-col1.metric(label="🎓 Average CGPA", value=f"{avg_cgpa:.2f}", help="Average of all students’ cumulative CGPA")
-col2.metric(label="📚 Average Attendance", value=f"{avg_attendance:.1f}", help="Average attendance rate among students")
-col3.metric(label="📝 Average Study Preparation", value=f"{avg_prep:.1f}", help="Average study preparation level")
-col4.metric(label="🎮 Average Gaming Time", value=f"{avg_gaming:.1f}", help="Average daily gaming hours")
-
-# Display cleaned dataset 
-st.dataframe(df)
+    # Display cleaned dataset 
+    st.subheader("Student Dataset")
+    st.dataframe(df)
