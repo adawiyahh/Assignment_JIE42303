fadb525b8aa4842f33e33962a3fcc95bc7445c24
adawiyahh <adawiyah2k06@gmail.com>
2025-11-02 04:54:00 +0800

    Create home.py

---
+++ home.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+st.set_page_config(page_title="Student Performance Dashboard")
+
+# Define Pages
+academic = st.Page("academic_background.py", title="Academic Performance", icon=":material/school:")
+socioeconomic = st.Page("socioeconomic_factors.py", title="Socioeconomic Insights", icon=":material/finance_chip:")
+behavior = st.Page("behavior_lifestyle.py", title="Behavioral Patterns", icon=":material/psychology:")
+
+# Navigation Menu
+pg = st.navigation(
+    {
+        "Analysis Pages": [academic, socioeconomic, behavior]
+    }
+)
+
+pg.run()
