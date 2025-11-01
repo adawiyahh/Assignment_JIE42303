4baaf974c2fd560c1d73a637beec18d0e13236be
adawiyahh <adawiyah2k06@gmail.com>
2025-11-02 05:29:39 +0800

    Update StudentPerformance.py

--- StudentPerformance.py
+++ StudentPerformance.py
@@ -1,24 +1,27 @@
 import streamlit as st
+from streamlit_option_menu import option_menu
 import StudentPerformance
 import socioeconomic_factors
 import behavior_lifestyle
 
-# Page config
-st.set_page_config(page_title="Student Performance Dashboard", layout="wide")
+st.set_page_config(page_title="Student Dashboard", layout="wide")
 
 # Sidebar menu
-st.sidebar.title("Menu")
-page = st.sidebar.radio(
-    "Navigate to:",
-    ["Student Performance", "Socioeconomic Factors", "Behavior Lifestyle"]
-)
+with st.sidebar:
+    selected = option_menu(
+        menu_title="Menu",  # Sidebar title
+        options=["Student Performance", "Socioeconomic Factors", "Behavior Lifestyle"],
+        icons=["school", "coin", "person-lines-fill"],  # Icons for each page
+        menu_icon="cast",  # Optional menu icon
+        default_index=0  # default page
+    )
 
-# Load the selected page
-if page == "Student Performance":
+# Load selected page
+if selected == "Student Performance":
     StudentPerformance.app()
-elif page == "Socioeconomic Factors":
+elif selected == "Socioeconomic Factors":
     socioeconomic_factors.app()
-elif page == "Behavior Lifestyle":
+elif selected == "Behavior Lifestyle":
     behavior_lifestyle.app()
 
 
