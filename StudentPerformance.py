6d6218396e3d32a5d146df5b82e070f1116d3918
adawiyahh <adawiyah2k06@gmail.com>
2025-11-02 06:49:07 +0800

    Update StudentPerformance.py

--- StudentPerformance.py
+++ StudentPerformance.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 def app():
-    st.set_page_config(page_title="Scientific Visualization")
+    st.markdown("## Assignment JIE42303: Scientific Visualization", unsafe_allow_html=True)
     st.title("Student Performance")
     st.subheader("Dataset: Student Performance Metrics")
     st.markdown("---")
