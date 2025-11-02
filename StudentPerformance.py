044576d5a01585168d562eefd012c28ba4b1c5a8
adawiyahh <adawiyah2k06@gmail.com>
2025-11-02 08:26:40 +0800

    Update StudentPerformance.py

--- StudentPerformance.py
+++ StudentPerformance.py
@@ -77,5 +77,60 @@ def app():
     According to the visualisation, there seems to be a slightly positive correlation between the HSC Score and the overall CGPA, indicating that students who have performed better academically in the past typically have higher university CGPAs.
     """)
 
+    st.markdown("---")
 
+    # Second visualization
+    st.markdown("### Visualization 2: SSC vs Overall CGPA")
+
+    st.subheader("Scatter Plot")
+
+    # Create Seaborn scatter plot
+    fig, ax = plt.subplots(figsize=(8, 6))
+    sns.scatterplot(data=df, x='SSC', y='Overall', color='#FF69B4', ax=ax)
+    ax.set_title('Scatter Plot of SSC vs Overall CGPA')
+    ax.set_xlabel('SSC Score')
+    ax.set_ylabel('Overall CGPA')
+    ax.grid(True)
+
+    # Display plot in Streamlit
+    plt.tight_layout()
+    st.pyplot(fig)
+
+    # Interpretation Section
+    st.markdown("### Interpretation")
+    st.write("""
+    This scatter plot shows that the SSC score has little predictive power for academic success because there seems to be a weak positive association between the SSC score and the overall CGPA.
+    """)
+
+    # Third visualization
+    st.markdown("### Visualization 3: SSC vs Overall CGPA")
+
+    st.subheader("Scatter Plot")
+
+    # Create Seaborn scatter plot
+    fig, ax = plt.subplots(figsize=(8, 6))
+    sns.boxplot(data=df, x='Semester', y='Overall', color='purple', ax=ax)
+    ax.set_title('Boxplot of Semester vs Overall CGPA')
+    ax.set_xlabel('Semester')
+    ax.set_ylabel('Overall CGPA')
+    ax.grid(True)
+    plt.xticks(rotation=45)
+    plt.tight_layout()
+
+    # Display plot in Streamlit
+    plt.tight_layout()
+    st.pyplot(fig)
+
+    # Calculate mean, median, and quartiles for Overall CGPA by Semester
+    semester_stats = df.groupby('Semester')['Overall'].describe()
+
+    # Display statistics in Streamlit
+    st.markdown("### Overall CGPA Statistics by Semester")
+    st.dataframe(semester_stats)
+
+    # Interpretation Section
+    st.markdown("### Interpretation")
+    st.write("""
+    Throughout the semesters, the students' **median Overall CGPA** stays high, but performance consistency often **decreases** after Semester 2 (highest median, smallest spread), with **greater variability** and more low-scoring outliers starting in **Semester 6**.
+    """)
     
